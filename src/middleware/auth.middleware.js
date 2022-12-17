8842976bddbbb87a7dac9330e7cbc4b099f35589
Luis Carlos Rangel <luiscrangel19@gmail.com>
2022-12-17 14:20:59 -0500

    add all update

---
+++ src/middleware/auth.middleware.js
@@ -0,0 +1,28 @@
+const JwtStrategy = require('passport-jwt').Strategy
+
+const ExtractJwt = require('passport-jwt').ExtractJwt
+const passport = require('passport')
+
+const jwtSecret = require('../../config').api.jwtSecret
+const {findUsersById} = require('../users/users.controllers')
+
+  const options = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme('jwt'),
+    secretOrKey: jwtSecret
+  }
+  passport.use(
+    new JwtStrategy(options, async(tokenDecoded, done)=> {
+      try {
+        const user = await findUsersById(tokenDecoded.id)
+        if (!user) {
+          return done(null, false )
+        }
+        return done(null, tokenDecoded)
+      } catch (error) {
+        return done(error, false)
+      }
+    })
+  )
+
+
+module.exports = passport
